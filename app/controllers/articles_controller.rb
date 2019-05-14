a2f4f0138b69f61a7b9418b7dd50841c4a66de4e
Fahad Ashraf <mfahadbinashraf@gmail.com>
2019-05-14 10:33:41 +0200

    added update feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,14 +11,28 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
         @article = Article.new(aritcle_params)
 
         if @article.save
             redirect_to @article
         else
-            # the render method is used so that the article object is passed to the new template
+            # the render method is used so that the article object is passed to the 'new' template
             render 'new' 
+        end
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+
+        if(@article.update(aritcle_params))
+            redirect_to @article
+        else
+            render 'edit'
         end
     end
     
